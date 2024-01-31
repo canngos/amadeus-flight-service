8578e7e0c2406e7c626914e2837aaea14a3b16ae
Can Baturlar <baturlarcan@gmail.com>
2024-01-31 15:44:16 +0300

    Register request created

---
+++ src/main/java/com/casestudy/amadeusflightservice/request/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.casestudy.amadeusflightservice.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    @NotBlank(message = "Name field cannot be empty")
+    private String name;
+    @NotBlank(message = "Last name field cannot be empty")
+    private String lastName;
+    @NotBlank(message = "Email field cannot be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+    @Size(min = 6, message = "Password should be at least 6 characters")
+    private String password;
+    private String address;
+
+}
