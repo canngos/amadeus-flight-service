c26afb52e914edfaf990b12e44e095cc3713bdde
Can Baturlar <baturlarcan@gmail.com>
2024-01-31 12:27:56 +0300

    Project initialized

---
+++ src/main/java/com/casestudy/amadeusflightservice/AmadeusFlightServiceApplication.java
@@ -0,0 +1,13 @@
+package com.casestudy.amadeusflightservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusFlightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmadeusFlightServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casestudy/amadeusflightservice/AmadeusFlightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casestudy.amadeusflightservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadeusFlightServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
