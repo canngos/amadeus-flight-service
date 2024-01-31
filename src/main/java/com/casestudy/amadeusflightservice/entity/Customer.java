b50160bb8757310ceb5fc29ff9e60e41907502ae
Can Baturlar <baturlarcan@gmail.com>
2024-01-31 15:46:58 +0300

    Customer entity created

---
+++ src/main/java/com/casestudy/amadeusflightservice/entity/Customer.java
@@ -0,0 +1,34 @@
+package com.casestudy.amadeusflightservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String address;
+    @Column(nullable = false)
+    private String password;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
