3d1fd0bbdc7238f71fb81fa74f74ef1ad449b9cb
Can Baturlar <baturlarcan@gmail.com>
2024-01-31 15:44:07 +0300

    Login response created

---
+++ src/main/java/com/casestudy/amadeusflightservice/response/body/LoginResponseBody.java
@@ -0,0 +1,17 @@
+package com.casestudy.amadeusflightservice.response.body;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginResponseBody {
+    private String token;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Europe/Istanbul")
+    private Date expirationDate;
+}
