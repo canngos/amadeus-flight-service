7649c5a68d8963edaa1cea6f6a79eecba9c7c5ee
Can Baturlar <baturlarcan@gmail.com>
2024-02-01 17:24:28 +0300

    Validation and parsing fixed

--- src/main/java/com/casestudy/amadeusflightservice/dto/FeignDto.java
+++ src/main/java/com/casestudy/amadeusflightservice/dto/FeignDto.java
@@ -3,7 +3,6 @@ package com.casestudy.amadeusflightservice.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -14,10 +13,8 @@ public class FeignDto {
     private String flightNumber;
     private String departureAirport;
     private String arrivalAirport;
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm", shape = JsonFormat.Shape.STRING)
     private LocalDateTime departureTime;
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm", shape = JsonFormat.Shape.STRING)
     private LocalDateTime arrivalTime;
     private String departureCity;
--- src/main/java/com/casestudy/amadeusflightservice/request/FlightRequest.java
+++ src/main/java/com/casestudy/amadeusflightservice/request/FlightRequest.java
@@ -1,28 +1,29 @@
 package com.casestudy.amadeusflightservice.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class FlightRequest {
-    @NotBlank(message = "There must be a departure airport")
+    @NotBlank(message = "flightNumber field cannot be empty")
+    private String flightNumber;
+    @NotNull(message = "There must be a departure airport")
     private Long departureAirport;
-    @NotBlank(message = "There must be an arrival airport")
+    @NotNull(message = "There must be an arrival airport")
     private Long arrivalAirport;
-    @NotBlank(message = "departureTime field cannot be empty")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @NotNull(message = "departureTime field cannot be empty")
+    @Future(message = "departureTime must be in the future")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm", shape = JsonFormat.Shape.STRING)
     private LocalDateTime departureTime;
-    @NotBlank(message = "arrivalTime field cannot be empty")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @NotNull(message = "arrivalTime field cannot be empty")
+    @Future(message = "arrivalTime must be in the future")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm", shape = JsonFormat.Shape.STRING)
     private LocalDateTime arrivalTime;
     @NotNull(message = "price field cannot be empty")
