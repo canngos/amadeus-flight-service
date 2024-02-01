1b29512ca7ef42b169f53b1b24218341f0093b8b
Can Baturlar <baturlarcan@gmail.com>
2024-02-01 14:32:46 +0300

    Airport dto created

---
+++ src/main/java/com/casestudy/amadeusflightservice/dto/AirportDto.java
@@ -0,0 +1,12 @@
+package com.casestudy.amadeusflightservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AirportDto {
+    private Long id;
+    private String name;
+    private String city;
+}
