d609ca886d83097d7898e01f8be98ea3eb858d0c
Can Baturlar <baturlarcan@gmail.com>
2024-02-01 14:34:33 +0300

    TransactionCodes added

--- src/main/java/com/casestudy/amadeusflightservice/exception/TransactionCode.java
+++ src/main/java/com/casestudy/amadeusflightservice/exception/TransactionCode.java
@@ -10,7 +10,12 @@ public enum TransactionCode {
     TOKEN_EXPIRED(102, "Token expired", HttpStatus.UNAUTHORIZED),
     TOKEN_INVALID(103, "Token invalid", HttpStatus.UNAUTHORIZED),
     INVALID_CREDENTIALS(104, "Invalid credentials", HttpStatus.UNAUTHORIZED),
-    EMAIL_EXISTS(105, "Email already exists", HttpStatus.CONFLICT);
+    EMAIL_EXISTS(105, "Email already exists", HttpStatus.BAD_REQUEST),
+    AIRPORT_ALREADY_EXISTS(106, "Airport already exists", HttpStatus.BAD_REQUEST),
+    AIRPORT_NOT_FOUND(107, "Airport not found", HttpStatus.NOT_FOUND),
+    SAME_AIRPORTS(108, "Departure and arrival airports cannot be the same", HttpStatus.BAD_REQUEST),
+    FLIGHT_NOT_FOUND(109, "Flight not found", HttpStatus.NOT_FOUND),
+    INVALID_DATE_FORMAT(110, "Invalid date format", HttpStatus.BAD_REQUEST);
 
     private final int id;
     private final String code;
