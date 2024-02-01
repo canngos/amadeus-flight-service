a9811ea945862d1fd5e01df866b2975ec0c4bd02
Can Baturlar <baturlarcan@gmail.com>
2024-02-01 15:36:34 +0300

    Feign client library added

--- src/main/java/com/casestudy/amadeusflightservice/AmadeusFlightServiceApplication.java
+++ src/main/java/com/casestudy/amadeusflightservice/AmadeusFlightServiceApplication.java
@@ -2,8 +2,10 @@ package com.casestudy.amadeusflightservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients // to enable feign client
 public class AmadeusFlightServiceApplication {
 
 	public static void main(String[] args) {
