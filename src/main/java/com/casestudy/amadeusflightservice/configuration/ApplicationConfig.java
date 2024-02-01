7b63d7f671eaa98d8765fb9c3ffbec7a717a5753
Can Baturlar <baturlarcan@gmail.com>
2024-02-01 15:36:10 +0300

    Configured for enabling schedule

--- src/main/java/com/casestudy/amadeusflightservice/configuration/ApplicationConfig.java
+++ src/main/java/com/casestudy/amadeusflightservice/configuration/ApplicationConfig.java
@@ -7,6 +7,7 @@ import com.casestudy.amadeusflightservice.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -18,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableScheduling
 public class ApplicationConfig {
 
     private final CustomerRepository customerRepository;
